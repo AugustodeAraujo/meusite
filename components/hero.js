8a8f1d3885a4a0d230b3e4df28a2a0b4f46bb6d9
AugustodeAraujo <poxaugustodantas@gmail.com>
2020-12-10 23:43:07 -0300

    Initial Commit

---
+++ components/hero.js
@@ -0,0 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Particles from 'react-particles-js';
+
+const Hero = () => {
+    return (
+        <>
+            <div className="jumbotron jumbotron-fluid has-bg-img2 hero d-flex flex-column justify-content-center m-0">
+                <Particles>
+                    <h1>Oi</h1>
+                </Particles>
+            </div>
+
+        </>
+    )
+}
+
+export default Hero
